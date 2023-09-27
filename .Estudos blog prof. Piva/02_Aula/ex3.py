e168a1c8934d334f85eaef45af4c6591bb6bbbb4
CremerAlbiero <raphael.cremer@hotmail.com>
2023-09-27 14:00:45 -0300

    add condição caso os números forem iguais.

--- .Estudos blog prof. Piva/02_Aula/ex3.py
+++ .Estudos blog prof. Piva/02_Aula/ex3.py
@@ -2,13 +2,16 @@
 
 from random import randint
 
-n1 = randint(0,100)
-n2 = randint(0,100)
+n1 = randint(0,50)
+n2 = randint(0,50)
 
 print(f"Números encontrados: {n1} e {n2}")
 
 if n1 > n2:
     print(f"O número {n1} é maior que {n2}")
+
+elif n1 == n2:
+    print(f"Os números são iguais")
 
 else:
     print(f"O número {n2} é maior que {n1}")
