737cb843e377ac8847025eb4930685b21991625b
CremerAlbiero <raphael.cremer@hotmail.com>
2023-09-27 14:44:24 -0300

    http://www.fatecead.com.br/alp/aula03_ppt.pdf

---
+++ .Estudos blog prof. Piva/03_Aula/ex1_expressoes.py
@@ -0,0 +1,12 @@
+from math import *
+
+expression_a = 2 > 3
+print(expression_a)
+
+expression_b = (6 > 8) or (3 < 7)
+print(expression_b)
+
+expression_d = not(2 < 3)
+print(expression_d)
+
+#False ; True ; False
