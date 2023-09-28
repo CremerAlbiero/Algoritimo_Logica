a300d440dd40b80fadd8f06eae7524118b85d192
CremerAlbiero <raphael.cremer@hotmail.com>
2023-09-28 15:03:32 -0300

    http://www.fatecead.com.br/alp/aula03_ppt.pdf

---
+++ .Estudos blog prof. Piva/03_Aula/ex2_expressoes.py
@@ -0,0 +1,20 @@
+#expressão a)
+a = 2
+b = 6
+c = 3
+d = 4
+e = 8
+f = 4
+
+resultado = (a + (b / c)) / (d - (e / f))
+print(resultado)
+
+
+
+#expressao b)
+
+from math import sqrt
+
+x = 2
+result = ((((2 * x**2) - (3 * x**(x+1))) / 2) + sqrt(x + 1) / x) / 2 *x
+print(result)
