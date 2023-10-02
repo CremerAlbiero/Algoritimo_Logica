fb9245518f2ae293c2cb93751b5e2cb87206b6a2
CremerAlbiero <raphael.cremer@hotmail.com>
2023-10-02 20:54:48 -0300

    http://www.fatecead.com.br/alp/aula04_ppt.pdf

--- .Estudos blog prof. Piva/04_aula/ex1_area-quadrilatero.py
+++ .Estudos blog prof. Piva/04_aula/ex1_area-quadrilatero.py
@@ -1,6 +1,6 @@
 #Calcular a área de um quadrilátero, com dois lados iguais. Para isso faz a leitura de dois numeros int, representando os lados e imrpime os valores lidos e a área calculdada.
 
-Altura = int(input("Digite o valor do lado A: "))
-Base = int(input("Digite o valor do lado B: "))
-Area = Base * Altura
-print(f"A área é: {Area}")
+altura = int(input("Digite o valor altura: "))
+base = int(input("Digite o valor da base: "))
+area = base * altura
+print(f"A área é: {area}. Altura é: {altura}. A base é: {base}")
