0d4a6b318baa2b5eab1d3be8d96d3eb3c7c6ca70
CremerAlbiero <raphael.cremer@hotmail.com>
2023-10-23 17:20:45 -0300

    matrizes

--- .Estudos blog prof. Piva/09_Aula/yt_exemplo01.py
+++
@@ -1,9 +0,0 @@
-lista_numeros = [4, 4, 6, 8, 9, 10, 12, 14]
-dir(lista_numeros)  #exibe todas as funcoes da class list.
-
-type(lista_numeros)
-#<class 'list'>
-
-lista_misturada = ['cachorro', 87.23, 6, True, "a", [1, 2, 3]]
-
-lista_vazia = []
--- .Estudos blog prof. Piva/09_Aula/yt_exemplo03.py
+++
@@ -1,4 +0,0 @@
-#funcao list()    built-in function.
-
-frase = "Um texto qualquer"
-lista_caracteres = list(frase)   #cria uma lista com cada caracter da string (inclusive os espaços).
