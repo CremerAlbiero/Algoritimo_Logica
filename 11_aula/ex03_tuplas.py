af7516c19702bca79627927e08cc089e8a7c78af
CremerAlbiero <140213736+CremerAlbiero@users.noreply.github.com>
2023-10-30 23:35:30 -0300

    Delete 11_aula directory

--- 11_aula/ex03_tuplas.py
+++
@@ -1,9 +0,0 @@
-lista1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-lista2 = [5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
-
-conjunto1 = set(lista1)
-conjunto2 = set(lista2)
-
-conjunto_main = conjunto1.union(conjunto2)
-
-print(f"Conjunto das duas listas: {conjunto_main}")
